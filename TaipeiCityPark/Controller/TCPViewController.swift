3b1119079160ace17ea5178c0e67e42fca8848fc
ChenYingYu <alan831007@gmail.com>
2018-06-29 13:45:37 +0800

    Create spotTableView and register SpotTableViewCell for reuse.

--- TaipeiCityPark/Controller/TCPViewController.swift
+++ TaipeiCityPark/Controller/TCPViewController.swift
@@ -10,10 +10,15 @@ import UIKit
 
 class TCPViewController: UIViewController {
     var spots = [Spot]()
+    @IBOutlet weak var spotTableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        spotTableView.delegate = self
+        spotTableView.dataSource = self
+        let nib = UINib(nibName: "SpotTableViewCell", bundle: nil)
+            spotTableView.register(nib, forCellReuseIdentifier: "Cell")
         TCPClient().taskForGETSpot() { (spots, error) in
             guard error == nil else {
                 print(error ?? "Found Error")
@@ -31,7 +36,19 @@ class TCPViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
 
+extension TCPViewController: UITableViewDelegate, UITableViewDataSource {
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return spots.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as? SpotTableViewCell else {
+            return UITableViewCell()
+        }
+        return cell
+    }
 }
 
