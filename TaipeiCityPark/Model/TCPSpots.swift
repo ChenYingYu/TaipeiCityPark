169249a2a92edbb96416cb8932df11279aa659cd
ChenYingYu <alan831007@gmail.com>
2018-06-29 11:34:07 +0800

    Create a model "TCPSpots", codable struct "Result" and "Spot" for parsing JSON data.

---
+++ TaipeiCityPark/Model/TCPSpots.swift
@@ -0,0 +1,37 @@
+//
+//  TCPSpots.swift
+//  TaipeiCityPark
+//
+//  Created by ChenAlan on 2018/6/29.
+//  Copyright © 2018年 ChenAlan. All rights reserved.
+//
+
+import Foundation
+
+struct Result: Codable {
+    let spots: [Spot]
+    let count: Int
+    enum CodingKeys: String, CodingKey {
+        case spots = "results"
+        case count
+    }
+}
+
+struct Spot: Codable {
+    let parkId: String
+    let parkName: String
+    let spotName: String
+    let yearBuilt: String
+    let openTime: String
+    let image: String
+    let introduction: String
+    enum CodingKeys: String, CodingKey {
+        case parkId = "_id"
+        case parkName = "ParkName"
+        case spotName = "Name"
+        case yearBuilt = "YearBuilt"
+        case openTime = "OpenTime"
+        case image = "Image"
+        case introduction = "Introduction"
+    }
+}
