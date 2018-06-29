6ac9a2359c7ee236a1667668e4e7fa8d82c11d59
ChenYingYu <alan831007@gmail.com>
2018-06-29 10:56:13 +0800

    Rename ViewController as TCPViewController.

--- TaipeiCityPark/TCPViewController.swift
+++ TaipeiCityPark/TCPViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  TCPViewController.swift
 //  TaipeiCityPark
 //
 //  Created by ChenAlan on 2018/6/29.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TCPViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
